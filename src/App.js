f679f7f296aa2fb4ec7b7dc10eefa47f239fecca
Abdul Moqueet <moqueetabdul@gmail.com>
2022-07-05 16:57:34 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { useSelector, useDispatch } from 'react-redux'
 
-import {incNumber, decNumber} from './actions'
+import { incNumber, decNumber } from './actions'
 
 function App() {
 
   const count = useSelector(state => state.counter)
   const dispatch = useDispatch()
 
-const handleAdd = () => {
-  dispatch(incNumber(1))
-}
+  const handleAdd = () => {
+    dispatch(incNumber(1))
+  }
 
-const handleSub = () => {
-  dispatch(decNumber(1))
-}
+  const handleSub = () => {
+    dispatch(decNumber(1))
+  }
 
   return (
     <div className="App">
